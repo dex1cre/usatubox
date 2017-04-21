c4748a259c0ef28537a7c1b9f1134d7dc3fcab4d
dex1cre <dex1cre@mail.ru>
2017-04-21 18:18:27 +0300

    hello

--- server.js
+++ server.js
@@ -199,6 +199,151 @@ var exit = function(req, res) {
 		return res.end("Ти что тут забыл?!!");
 }
 
+var ninh = function(req, res) {
+	db.serialize(function() {
+		return db.all("SELECT * FROM category", function(err, rows) {
+			if (err)
+				console.log(err);
+			return res.render("nikita_idi_na_her", {
+				category: rows,
+			});
+		});
+	});
+}
+
+var ninh_zak = function(req, res) {
+	db.serialize(function() {
+		return db.all("SELECT * FROM users", function(err, rows) {
+			if (err)
+				console.log(err);
+			return res.render('ninh-zak', {
+				users: rows
+			});
+		});
+	});
+}
+
+var ninh_zak_name = function(req, res) {
+	var id_user = req.query.id;
+	db.serialize(function() {
+		return db.all('SELECT * FROM orders WHERE id_user=$id AND status=1', {
+			$id: id_user
+		}, function(err, rows) {
+			if (err)
+				console.log(err);
+
+				var mass = [];
+				var mass2 = [];
+				var flag = false;
+				var index = 0;
+
+				for (var i=0; i < rows.length; i++) {
+					for (var j=0; j < mass2.length; j++) {
+						if (mass2[j] == rows[i].id_tov) {
+							flag = true;
+							index = j;
+							break;
+						}
+					}
+
+					if (flag) {
+						var count = rows[i].count;
+						mass[index].count += count;
+					} else {
+						mass.push(rows[i]);
+						mass2.push(rows[i].id_tov);
+					}
+				}
+
+				return db.all("SELECT * FROM tov", function(err2, rows2) {
+					if (err2)
+						console.log(err2);
+					return res.render("ninh-zak", {
+						orders: mass,
+						tovars: rows2,
+					})
+				})
+		});
+	});
+}
+
+var nh_tov = function(req, res) {
+	db.serialize(function() {
+		return db.all("SELECT * FROM tov", function(err, rows) {
+			if (err)
+				console.log(err);
+
+			return res.render("nh-tov", {
+				tovars: rows
+			})
+		});
+	});
+}
+
+var ninh_new = function(req, res) {
+	if (req.body.name != "" &&
+			req.body.description != "" &&
+			req.body.price != "" &&
+			req.body.count != "" &&
+			req.body.category != "" &&
+			req.body.img != "")
+			db.serialize(function() {
+				return db.run("INSERT INTO tov(name, description, price, count, category, img) VALUES($name, $description, $price, $count, $category, $img)", {
+					$name: req.body.name,
+					$description: req.body.description,
+					$price: req.body.price,
+					$count: req.body.count,
+					$category: req.body.category,
+					$img: req.body.img
+				}, function(err) {
+					if (err)
+						console.log(err);
+					return res.redirect("/ninh");
+				});
+			});
+	else
+		return res.end("Не все поля заполнены");
+}
+
+var find_tov = function(req, res) {
+	db.serialize(function() {
+		return db.all("SELECT * FROM tov WHERE id=$id", {
+			$id: req.body.id
+		}, function(err, rows) {
+			if (err)
+				console.log(err);
+			if (rows[0] && rows[0].id != "")
+				return res.send(`
+					<html>
+					<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css", rel="stylesheet", integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u", crossorigin="anonymous">
+					<body>
+					<div class="container">
+						` + rows[0].name + `<br>
+						<form method="post" action="/find-delete-tov">
+							<input type="text" name="id" value="` + rows[0].id + `">
+							<button class="btn btn-danger">Удалить</button>
+						</form>
+					</div>
+					</body>
+					</html>
+					`);
+			else
+				return res.end("Ничего не найдено!");
+		});
+	});
+}
+
+var find_delete_tov = function(req, res) {
+	db.serialize(function() {
+		return db.run("DELETE FROM tov WHERE id=$id", {
+			$id: req.body.id
+		}, function(err) {
+			if (err)
+				console.log(err);
+			return res.redirect("/ninh");
+		});
+	});
+}
 
 data = {
 	"/": index,
@@ -210,6 +355,13 @@ data = {
 	'/enter-site': enter_site,
 	'/to-basket-see': to_basket_see,
 	'/exit': exit,
+	'/ninh': ninh,
+	'/ninh-zak': ninh_zak,
+	'/ninh-new': ninh_new,
+	'/ninh_zak/name': ninh_zak_name,
+	'/nh-tov': nh_tov,
+	'/find-tov': find_tov,
+	'/find-delete-tov': find_delete_tov,
 }
 
 exports.data = data;
